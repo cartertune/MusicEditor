3be3868331b6c62a5b7457125de591ab8e189140
fshikara <shikara.f@husky.neu.edu>
2017-06-16 10:34:18 -0400

    Midi View Implementation begins.   Sequencer used instead of Synthesizer. More work now, but cleaner  start and stop. Sequencer Boilerplate added.
    Todo: clean, and divide into helpers
          ->Implement note recording

--- src/cs3500/music/view/MidiViewImpl.java
+++ src/cs3500/music/view/MidiViewImpl.java
@@ -1,71 +1,93 @@
 package cs3500.music.view;
 
+import cs3500.music.model.MusicOperations;
 import javax.sound.midi.*;
 
-/**
+/** todo: update javadoc
  * A skeleton for MIDI playback
  */
 public class MidiViewImpl implements ViewInterface {
-  private final Synthesizer synth;
-  private final Receiver receiver;
+  private Receiver receiver;
+  private Sequencer sequencer = null;
 
+  //todo: mod, overload constructor? - one with model and [one empty?]
   public MidiViewImpl() throws MidiUnavailableException {
-    this.synth = MidiSystem.getSynthesizer();
-    this.receiver = synth.getReceiver();
-    this.synth.open();
-  }
-  /**
-   * Relevant classes and methods from the javax.sound.midi library:
-   * <ul>
-   *  <li>{@link MidiSystem#getSynthesizer()}</li>
-   *  <li>{@link Synthesizer}
-   *    <ul>
-   *      <li>{@link Synthesizer#open()}</li>
-   *      <li>{@link Synthesizer#getReceiver()}</li>
-   *      <li>{@link Synthesizer#getChannels()}</li>
-   *    </ul>
-   *  </li>
-   *  <li>{@link Receiver}
-   *    <ul>
-   *      <li>{@link Receiver#send(MidiMessage, long)}</li>
-   *      <li>{@link Receiver#close()}</li>
-   *    </ul>
-   *  </li>
-   *  <li>{@link MidiMessage}</li>
-   *  <li>{@link ShortMessage}</li>
-   *  <li>{@link MidiChannel}
-   *    <ul>
-   *      <li>{@link MidiChannel#getProgram()}</li>
-   *      <li>{@link MidiChannel#programChange(int)}</li>
-   *    </ul>
-   *  </li>
-   * </ul>
-   * @see <a href="https://en.wikipedia.org/wiki/General_MIDI">
-   *   https://en.wikipedia.org/wiki/General_MIDI
-   *   </a>
-   */
-
-  public void playNote() throws InvalidMidiDataException {
-    MidiMessage start = new ShortMessage(ShortMessage.NOTE_ON, 0, 60, 64);
-    MidiMessage stop = new ShortMessage(ShortMessage.NOTE_OFF, 0, 60, 64);
-    this.receiver.send(start, -1);
-    this.receiver.send(stop, this.synth.getMicrosecondPosition() + 200000);
-    
-    /* 
-    The receiver does not "block", i.e. this method
-    immediately moves to the next line and closes the 
-    receiver without waiting for the synthesizer to 
-    finish playing. 
-    
-    You can make the program artificially "wait" using
-    Thread.sleep. A better solution will be forthcoming
-    in the subsequent assignments.
-    */
-    this.receiver.close(); // Only call this once you're done playing *all* notes
+    //null check
   }
 
-  @Override
-  public void initialize() {
 
+  // todo:delete this method, currently serving as reference
+//  public void playNote() throws InvalidMidiDataException {
+//    MidiMessage start = new ShortMessage(ShortMessage.NOTE_ON, 0, 60, 64);
+//    MidiMessage stop = new ShortMessage(ShortMessage.NOTE_OFF, 0, 60, 64);
+//    this.receiver.send(start, -1);
+//    //this.receiver.send(stop, this.synth.getMicrosecondPosition() + 200000);
+//
+//    /*
+//    The receiver does not "block", i.e. this method
+//    immediately moves to the next line and closes the
+//    receiver without waiting for the synthesizer to
+//    finish playing.
+//
+//    You can make the program artificially "wait" using
+//    Thread.sleep. A better solution will be forthcoming
+//    in the subsequent assignments.
+//    */
+//    this.receiver.close(); // Only call this once you're done playing *all* notes
+//  }
+
+
+  //todo: delete, move internals to initialize or private load music method
+  public void testSequencer(MusicOperations model) throws InvalidMidiDataException, MidiUnavailableException {
+    sequencer = MidiSystem.getSequencer(); //default sequencer
+    sequencer.open();
+
+    this.receiver = sequencer.getReceiver(); //todo: Move to constructor
+
+        //test receiver
+    //System.out.print(sequencer.getReceiver()); //todo: delete
+
+
+
+    //create sequence
+    Sequence sequence = null;
+    sequence = new Sequence(Sequence.PPQ, 10); //10 pulses per quarter note
+    sequence.createTrack();
+    //System.out.print(sequencer.getTracks()); //todo:delete
+
+
+    sequencer.setSequence(sequence);
+
+    //Only when about to record
+    sequencer.recordEnable(sequence.getTracks()[0], 0);
+
+    //---------------------------------------------------------------------------------------------
+    // ^cruff above is setup.   V Cruff below is actual music recording, Midi messages
+
+
+    //Breakdown: For each note, send 2 ShortMessages, A start and stop.
+    this.receiver.send(new ShortMessage(ShortMessage.NOTE_ON, 0, 64, 64), 0);
+    this.receiver.send(new ShortMessage(ShortMessage.NOTE_OFF, 0, 64, 64), 20);
+
+
+    //Ideally, Iterate through a list of notes.
+
+    //Alternatively, get last note time, and iterate based on time from 0 to that.
+
+    sequencer.close(); //when done recording, makes sequencer available for use
+    sequencer.start(); //start play
+  } //todo: divide into private helpers.
+
+
+  //todo: implement, save converted sequence. Should the method be split into private load and play?
+  private void loadPiece(MusicOperations piece) { //todo: should this be initialize?
+
+
+  }
+
+
+  @Override //todo: whatever this does
+  public void initialize() { //todo: note: using a sequencer makes starting and stopping easier and cleaner
+    //sqr.start()
   }
 }
---
+++ test/cs3500/music/view/MidiViewImplTest.java
@@ -0,0 +1,20 @@
+package cs3500.music.view;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * todo: javadoc
+ */
+public class MidiViewImplTest {
+
+  @Test
+  public void testSequencer() throws Exception {
+    MidiViewImpl view = new MidiViewImpl(); //change to ViewInterface for real deal
+
+    view.testSequencer(null);
+
+  }
+
+}
