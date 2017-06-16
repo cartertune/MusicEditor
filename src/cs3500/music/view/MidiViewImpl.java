551f1e1528f6dea001c46c1f830b8135e44a9618
faris <shikara.f@husky.neu.edu>
2017-06-16 12:50:10 -0400

    Implementation complete Todo: javadoc, structure cleanup.

--- src/cs3500/music/view/MidiViewImpl.java
+++ src/cs3500/music/view/MidiViewImpl.java
@@ -1,7 +1,11 @@
 package cs3500.music.view;
 
+import cs3500.music.model.INote;
 import cs3500.music.model.MusicOperations;
+import cs3500.music.model.Note;
+
 import javax.sound.midi.*;
+import java.util.ArrayList;
 
 /** todo: update javadoc
  * A skeleton for MIDI playback
@@ -11,83 +15,58 @@ public class MidiViewImpl implements ViewInterface {
   private Sequencer sequencer = null;
 
   //todo: mod, overload constructor? - one with model and [one empty?]
-  public MidiViewImpl() throws MidiUnavailableException {
-    //null check
+  public MidiViewImpl(MusicOperations model) throws MidiUnavailableException {
+    sequencer = MidiSystem.getSequencer(); //default sequencer
+
+    try {
+      this.loadPiece(model); //Create sequence from model
+    } catch (InvalidMidiDataException e) {
+      e.printStackTrace();
+      throw new IllegalArgumentException("Invalid model: " + e.getMessage());
+    }
   }
 
-
-  // todo:delete this method, currently serving as reference
-//  public void playNote() throws InvalidMidiDataException {
-//    MidiMessage start = new ShortMessage(ShortMessage.NOTE_ON, 0, 60, 64);
-//    MidiMessage stop = new ShortMessage(ShortMessage.NOTE_OFF, 0, 60, 64);
-//    this.receiver.send(start, -1);
-//    //this.receiver.send(stop, this.synth.getMicrosecondPosition() + 200000);
-//
-//    /*
-//    The receiver does not "block", i.e. this method
-//    immediately moves to the next line and closes the
-//    receiver without waiting for the synthesizer to
-//    finish playing.
-//
-//    You can make the program artificially "wait" using
-//    Thread.sleep. A better solution will be forthcoming
-//    in the subsequent assignments.
-//    */
-//    this.receiver.close(); // Only call this once you're done playing *all* notes
-//  }
-
-
-  //todo: delete, move internals to initialize or private load music method
-  public void testSequencer(MusicOperations model) throws InvalidMidiDataException, MidiUnavailableException {
-    sequencer = MidiSystem.getSequencer(); //default sequencer
+  //todo: javadoc
+  private void loadPiece(MusicOperations piece) throws InvalidMidiDataException, MidiUnavailableException {
+    //Boiler Plate
     sequencer.open();
-
     this.receiver = sequencer.getReceiver(); //todo: Move to constructor
-
-        //test receiver
-    //System.out.print(sequencer.getReceiver()); //todo: delete
-
-
-
-    //create sequence
-    Sequence sequence = null;
-    sequence = new Sequence(Sequence.PPQ, 10); //10 pulses per quarter note
+    //todo: tempo in Pulses per tick per quarter note as time resolution in new sequence
+    Sequence sequence = new Sequence(Sequence.PPQ, 10); //10 pulses per quarter note
     sequence.createTrack();
-    //System.out.print(sequencer.getTracks()); //todo:delete
-
-
     sequencer.setSequence(sequence);
-
-    //Only when about to record
     sequencer.recordEnable(sequence.getTracks()[0], 0);
 
     //---------------------------------------------------------------------------------------------
     // ^cruff above is setup.   V Cruff below is actual music recording, Midi messages
 
-
-    //Breakdown: For each note, send 2 ShortMessages, A start and stop.
-    this.receiver.send(new ShortMessage(ShortMessage.NOTE_ON, 0, 64, 64), 0);
-    this.receiver.send(new ShortMessage(ShortMessage.NOTE_OFF, 0, 64, 64), 20);
-
-
     //Ideally, Iterate through a list of notes.
-
     //Alternatively, get last note time, and iterate based on time from 0 to that.
+    for (int time = 0; time < piece.maxBeatNum(); time++) {
+      ArrayList<INote> notes = piece.getNotesAt(time);
+      if (!notes.isEmpty()) { //there are notes here
+        for (INote note:notes) { //For each note at this time,send 2 messages
+
+          this.receiver.send(new ShortMessage(ShortMessage.NOTE_ON,
+                  note.getInstrument(), //Instrument midi value
+                  note.value(),
+                  note.getVolume()),
+                  time + note.getDuration()); // Time stamp
+
+          this.receiver.send(new ShortMessage(ShortMessage.NOTE_OFF,
+                          note.getInstrument(), //Instrument midi value
+                          note.value(),
+                          note.getVolume()),
+                  time + note.getDuration()); // Time stamp
+        }
+      }
+    }
 
     sequencer.close(); //when done recording, makes sequencer available for use
-    sequencer.start(); //start play
-  } //todo: divide into private helpers.
-
-
-  //todo: implement, save converted sequence. Should the method be split into private load and play?
-  private void loadPiece(MusicOperations piece) { //todo: should this be initialize?
-
-
   }
 
-
-  @Override //todo: whatever this does
-  public void initialize() { //todo: note: using a sequencer makes starting and stopping easier and cleaner
-    //sqr.start()
+  @Override
+  public void initialize() {
+    sequencer.start(); // start playing loaded music
   }
 }
